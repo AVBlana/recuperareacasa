387402e0e95f57213eb2c04f39e54c46e0a9f520
AVBlana <71121584+AVBlana@users.noreply.github.com>
2024-02-22 17:59:11 +0200

    changed content, semi-fixed services slider, still working on arrows

--- src/theme/components/organisms/Services/index.tsx
+++ src/theme/components/organisms/Services/index.tsx
@@ -12,95 +12,266 @@ const Services = () => {
   const services = [
     {
       label: "200 Ron",
-      title: "Kinetoterapie de recuperare",
+      title: "Kinetoterapie de Recuperare",
       svg: "/assets/headPain.svg",
       image: "/assets/img1.jpg",
-      desc: "Echipa noastră de terapeuți experimentați integrează o gamă diversă de tehnici manuale pentru a vă oferi o terapie holistică. De la tehnici de chiropractică și streching, pană la masaj terapeutic și alte metode manuale avansate, abordăm individual nevoile dumneavoastră. Acestă combinație expertă de practici manuale contribuie la ameliorarea durerilor, îmbunătățirea flexibilității și promovarea stării generale de bine. Descoperiți beneficiile unei terapii manuale comprehensive pentru sănătatea și mobilitatea dumneavoastră.",
+      desc: (
+        <>
+          Echipa noastră de terapeuți experimentați integrează o gamă diversă de
+          tehnici manuale pentru a vă oferi o terapie holistică.
+          <br />
+          <br />
+          De la tehnici de chiropractică și stretching, până la masaj terapeutic
+          și alte metode manuale avansate, abordăm individual nevoile
+          dumneavoastră.
+          <br />
+          <br />
+          Acestă combinație expertă de practici manuale contribuie la
+          ameliorarea durerilor, îmbunătățirea flexibilității și promovarea
+          stării generale de bine.
+          <br />
+          <br />
+          Descoperiți beneficiile unei terapii manuale comprehensive pentru
+          sănătatea și mobilitatea dumneavoastră.
+        </>
+      ),
     },
     {
       label: "150 Ron",
-      title: "Masaj sportiv și de recuperare",
+      title: "Masaj Sportiv și de Recuperare",
       svg: "/assets/legPain.svg",
       image: "/assets/img2.jpg",
-      desc: "Experimentați beneficiile revitalizante ale masajului sportiv și de recuperare oferit de echipa noastră dedicată. Proiectat special pentru atleți și persoane active, acest tip de masaj nu doar calmează tensiunea musculară, ci și accelerează procesul de recuperare după antrenamente intense sau leziuni minore. Prin tehnici experte și personalizate, masajul sportiv și de recuperare pe care-l oferim vizează restabilirea flexibilitătii, recurerea inflamației și îmbunatățirea performanței generale.",
+      desc: (
+        <>
+          Experimentați beneficiile revitalizante ale masajului sportiv și de
+          recuperare oferit de echipa noastră dedicată.
+          <br />
+          <br />
+          Proiectat special pentru atleți și persoane active, acest tip de masaj
+          nu doar calmează tensiunea musculară, ci și accelerează procesul de
+          recuperare după antrenamente intense sau leziuni minore.
+          <br />
+          <br />
+          Prin tehnici experte și personalizate, masajul sportiv și de
+          recuperare pe care-l oferim vizează restabilirea flexibilității,
+          reducerea inflamației și îmbunătățirea performanței generale.
+        </>
+      ),
     },
     {
       label: "150 Ron",
-      title: "Masaj de relaxare",
+      title: "Masaj de Relaxare",
       svg: "/assets/torsoPain.svg",
       image: "/assets/img3.jpg",
-      desc: "Bucurați-vă de o evadare totală din stresul zilnic cu serviciile noastre de masaj de relaxare. Prin tehnici fine și atenție delicată, vă oferim o experiență de relaxare profundă, stimulând simultan producția de endorfine pentru o stare generală de bine. Lasati-vă în mâinile experților noștri pentru a vă elibera de tensiunea fizică și mentală, promovând echilibrul și liniștea interioară.",
+      desc: (
+        <>
+          Bucurați-vă de o evadare totală din stresul zilnic cu serviciile
+          noastre de masaj de relaxare.
+          <br />
+          <br />
+          Prin tehnici fine și atenție delicată, vă oferim o experiență de
+          relaxare profundă, stimulând simultan producția de endorfine pentru o
+          stare generală de bine.
+          <br />
+          <br />
+          Lăsați-vă în mâinile experților noștri pentru a vă elibera de
+          tensiunea fizică și mentală, promovând echilibrul și liniștea
+          interioară.
+        </>
+      ),
     },
     {
       label: "30 Ron",
-      title: "Aplicare Kinesio tape",
+      title: "Aplicare Kinesio Tape",
       svg: "/assets/headPain.svg",
       image: "/assets/img4.jpg",
-      desc: "Descoperiți beneficiile terapiei cu bandă kinesiologică în cadrul serviciilor nostre specializate. Echipa noastră calificată aplică bandajul kinesiologic cu precizie pentru a sprijini și a stabiliza mușchii și articulațiile, reducând durerea și îmbunatățind funcționaliatea. Indiferent dacă aveți nevoie de susținere în recuperarea după o leziune sau de optimizare a performaței sportive, utilizarea bandajului kinesiologic poate fi o soluție eficientă și non-invazivă.",
+      desc: (
+        <>
+          Descoperiți beneficiile terapiei cu bandă kinesiologică în cadrul
+          serviciilor noastre specializate.
+          <br />
+          <br />
+          Echipa noastră calificată aplică bandajul kinesiologic cu precizie
+          pentru a sprijini și a stabiliza mușchii și articulațiile, reducând
+          durerea și îmbunătățind funcționalitatea.
+          <br />
+          <br />
+          Indiferent dacă aveți nevoie de susținere în recuperarea după o
+          leziune sau de optimizare a performanței sportive, utilizarea
+          bandajului kinesiologic poate fi o soluție eficientă și non-invazivă.
+        </>
+      ),
     },
     {
       label: "30 Ron",
-      title: "Eletroterapie",
+      title: "Electroterapie",
       svg: "/assets/headPain.svg",
       image: "/assets/img5.jpg",
-      desc: "Experimentați inovația în recuperare și terapie cu serviciile noastre de electroterapie. Folosind tehnologii avansate, oferim tratamente personalizate care utilizează stimularea electircă pentru a ameliora durerea, îmbunătăți funcția musculară și accelera procesul de vindecare. Echipa noastră de specialiști vă ghidează în această experiență sigură și eficientă, aducând beneficiile electroterapiei pentru o recuperare optimă.",
+      desc: (
+        <>
+          Experimentați inovația în recuperare și terapie cu serviciile noastre
+          de electroterapie.
+          <br />
+          <br />
+          Folosind tehnologii avansate, oferim tratamente personalizate care
+          utilizează stimularea electrică pentru a ameliora durerea, îmbunătăți
+          funcția musculară și accelera procesul de vindecare.
+          <br />
+          <br />
+          Echipa noastră de specialiști vă ghidează în această experiență sigură
+          și eficientă, aducând beneficiile electroterapiei pentru o recuperare
+          optimă.
+        </>
+      ),
     },
     {
       label: "100 Ron",
       title: "Dry Needling",
       svg: "/assets/headPain.svg",
       image: "/assets/img6.jpg",
-      desc: "Descoperiți beneficiile terapiei cu ac uscat (dry needling) în cadrul serviciilor noastre specializate. Cu ajutorul acestei tehnici precise, terapeuții noștri introduc acupunctura în puncte trigger ale mușchilor, eliberând tensiunea și ameliorând disconfortul. Procedura non-invazivă contribuie la relaxarea mușchilor, îmbunătățirea circulației sanguine și accelerarea procesului de vindecare. Redescoperiți mobilitatea și confortul într-o manieră eficientă și personalizată.",
+      desc: (
+        <>
+          Descoperiți beneficiile terapiei cu ac uscat (dry needling) în cadrul
+          serviciilor noastre specializate.
+          <br />
+          <br />
+          Cu ajutorul acestei tehnici precise, terapeuții noștri introduc
+          acupunctura în puncte trigger ale mușchilor, eliberând tensiunea și
+          ameliorând disconfortul.
+          <br />
+          <br />
+          Procedura non-invazivă contribuie la relaxarea mușchilor,
+          îmbunătățirea circulației sanguine și accelerarea procesului de
+          vindecare.
+          <br />
+          <br />
+          Redescoperiți mobilitatea și confortul într-o manieră eficientă și
+          personalizată.
+        </>
+      ),
     },
     {
       label: "100 Ron",
-      title: "Evaluare neuro-musculo-scheletală",
+      title: "Evaluare Neuro-Musculo-Scheletală",
       svg: "/assets/headPain.svg",
       image: "/assets/img6.jpg",
-      desc: "Beneficiați de o evaluare neuro-musculo-scheletală detaliată și personalizată în cadrul serviciilor noastre. Echipa noastră de specialiști întelege complexitatea conexiunii dintre sistemul nervos, mușchi și schelet și utilizează metode avansate pentru a analiza funcționalitatea și potențialele probleme. Prin această evaluare meticuloasă, putem indetifica disfuncționalități și devolta un plan de tratament adaptat nevoilor dumneavoastră individuale, asigurând recuperare și îmbunătățirea stării de sănătate generală.",
+      desc: (
+        <>
+          Beneficiați de o evaluare neuro-musculo-scheletală detaliată și
+          personalizată în cadrul serviciilor noastre.
+          <br />
+          <br />
+          Echipa noastră de specialiști înțelege complexitatea conexiunii dintre
+          sistemul nervos, mușchi și schelet și utilizează metode avansate
+          pentru a analiza funcționalitatea și potențialele probleme.
+          <br />
+          <br />
+          Prin această evaluare meticuloasă, putem identifica disfuncționalități
+          și dezvolta un plan de tratament adaptat nevoilor dumneavoastră
+          individuale, asigurând recuperare și îmbunătățirea stării de sănătate
+          generală.
+        </>
+      ),
     },
     {
       label: "100 Ron",
-      title: "Eliberare fascială IATSM",
+      title: "Eliberare Fascială IATSM",
       svg: "/assets/headPain.svg",
       image: "/assets/img6.jpg",
-      desc: "Se integrează după necesitate în serviciul de terapie manuală. IATSMT, o componentă valoroasă a terapiei noastre manuale, utilizează instrumente specializate pentru a îmbunătății țesuturile moi. Integrată în abordarea noastră holistică, această tehnică constribuie la eliminarea aderențelor, îmbunătățește circulația și susține vindecarea. Descoperiți eficiența acestei terapii în cadrul tratamentului nostru comprehensiv de terapie manuală.",
+      desc: (
+        <>
+          Se integrează după necesitate în serviciul de terapie manuală.
+          <br />
+          <br />
+          IATSMT, o componentă valoroasă a terapiei noastre manuale, utilizează
+          instrumente specializate pentru a îmbunătăți țesuturile moi.
+          <br />
+          <br />
+          Integrată în abordarea noastră holistică, această tehnică contribuie
+          la eliminarea aderențelor, îmbunătățirea circulației și susținerea
+          vindecării.
+          <br />
+          <br />
+          Descoperiți eficiența acestei terapii în cadrul tratamentului nostru
+          comprehensiv de terapie manuală.
+        </>
+      ),
     },
     {
       label: "100 Ron",
       title: "Cupping",
       svg: "/assets/headPain.svg",
       image: "/assets/img6.jpg",
-      desc: "Se integrează după necesitate în serviciul de terapie manuală. Descperă beneficiile terapiei cu ventuze în cadrul serviciilor noastre de recuperare. Această metodă milenară stimulează circulația sanguină și relaxează țesuturile, contribuind la ameliorarea tensiunii musculare și recuperarea rapidă a mobilității. Experimentează terapia cu ventuze pentru o revitalizare profundă a corpului și o cale eficientă către recuperare.",
+      desc: (
+        <>
+          Se integrează după necesitate în serviciul de terapie manuală.
+          <br />
+          <br />
+          Descoperiți beneficiile terapiei cu ventuze în cadrul serviciilor
+          noastre de recuperare.
+          <br />
+          <br />
+          Această metodă milenară stimulează circulația sanguină și relaxează
+          țesuturile, contribuind la ameliorarea tensiunii musculare și
+          recuperarea rapidă a mobilității.
+          <br />
+          <br />
+          Experimentați terapia cu ventuze pentru o revitalizare profundă a
+          corpului și o cale eficientă către recuperare.
+        </>
+      ),
     },
     {
       label: "100 Ron",
       title: "Flossing",
       svg: "/assets/headPain.svg",
       image: "/assets/img6.jpg",
-      desc: "Se integrează după necesitate în serviciul de terapie manuală. Integrăm în procesul nostru de recuperare tehnica avansată a 'blood flow restriction' pentru a maximiza eficacitatea terapiei. Această metodă inovatoare, folosind restrictia controlată a fluxului sanguin, intensifică antrenamentele musculare și accelerează recuperarea. Experimentează synergia dintre flossing și blade flow restriction pentru rezultate remarcabile în îmbunătățirea flexibiliății și optimizarea performanțelor tale.",
+      desc: (
+        <>
+          Se integrează după necesitate în serviciul de terapie manuală.
+          <br />
+          <br />
+          Integrăm în procesul nostru de recuperare tehnica avansată a 'blood
+          flow restriction' pentru a maximiza eficacitatea terapiei.
+          <br />
+          <br />
+          Această metodă inovatoare, folosind restricția controlată a fluxului
+          sanguin, intensifică antrenamentele musculare și accelerează
+          recuperarea.
+          <br />
+          <br />
+          Experimentați synergia dintre flossing și blade flow restriction
+          pentru rezultate remarcabile în îmbunătățirea flexibilității și
+          optimizarea performanțelor tale.
+        </>
+      ),
     },
   ];
 
-  const itemsPerPage = 5;
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextArrowClicked, setNextArrowClicked] = useState(false);
 
-  const totalItems = services.length;
-  const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-  const handleNextPage = () => {
-    setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
+  const handleNextCard = () => {
+    if (currentIndex < services.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+      setNextArrowClicked(true);
+    }
   };
 
-  const handlePrevPage = () => {
-    setCurrentPage((prevPage) =>
-      prevPage === 0 ? totalPages - 1 : prevPage - 1
-    );
+  const handlePrevCard = () => {
+    if (nextArrowClicked || currentIndex > 0) {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? services.length - 1 : prevIndex - 1
+      );
+      setNextArrowClicked(false);
+    }
   };
 
-  const startIndex = currentPage * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const visibleServices = services.slice(startIndex, endIndex);
+  useEffect(() => {
+    // You may want to add additional logic here when currentIndex changes.
+    // For example, you could trigger some side effects or update other parts of your UI.
+    console.log("Current index changed:", currentIndex);
+  }, [currentIndex]);
 
   return (
     <StyledServicesBox>
@@ -120,37 +291,45 @@ const Services = () => {
             </Text>
             <Text style={{ width: 200 }}>
               Specializăm tratamentul kineto pentru a acoperi atât aspectele
-              fizice, cât și cele psihice. Echipa noastră de kinetoterapeuți
-              oferă servicii personalizate pentru o recuperare completă.
+              fizice, cât și cele psihice.
+              <br />
+              <br />
+              Echipa noastră de kinetoterapeuți oferă servicii personalizate
+              pentru o recuperare completă.
             </Text>
           </Box>
 
           <Flex style={{ marginLeft: -20 }}>
-            <StyledArrowBox
-              style={{ padding: 20, cursor: "pointer" }}
-              onClick={handlePrevPage}
-            >
+            <StyledArrowBox onClick={handlePrevCard} hidden={!nextArrowClicked}>
               <ArrowLeftIcon size={40} fill={theme.color.secondary} />
             </StyledArrowBox>
-            <StyledArrowBox
-              style={{ padding: 20, cursor: "pointer" }}
-              onClick={handleNextPage}
-            >
-              <ArrowRightIcon size={40} fill={theme.color.secondary} />
-            </StyledArrowBox>
+            {currentIndex < services.length - 1 && (
+              <StyledArrowBox onClick={handleNextCard}>
+                <ArrowRightIcon size={40} fill={theme.color.secondary} />
+              </StyledArrowBox>
+            )}
           </Flex>
         </Box>
       </StyledControlBox>
-      <StyledServiceCardsContainer itemsPerPage={itemsPerPage}>
-        {visibleServices.map((service, index) => (
-          <ServiceCard key={index} service={service} />
-        ))}
-      </StyledServiceCardsContainer>
+      <StyledServiceCardsContainerWrapper>
+        <StyledServiceCardsContainer
+          currentIndex={currentIndex}
+          services={services}
+        >
+          {services.map((service, index) => (
+            <ServiceCard key={index} service={service} />
+          ))}
+        </StyledServiceCardsContainer>
+      </StyledServiceCardsContainerWrapper>
     </StyledServicesBox>
   );
 };
 
-const StyledArrowBox = styled.div`
+const StyledServiceCardsContainerWrapper = styled.div`
+  overflow: hidden;
+`;
+
+const StyledArrowBox = styled.div<{ hidden?: boolean }>`
   display: flex;
   align-items: center;
   padding: 20px;
@@ -178,7 +357,6 @@ const StyledServicesBox = styled.div`
   padding-bottom: 100px;
   padding-left: 60px;
   padding-right: 0px;
-  overflow: hidden;
 `;
 
 const StyledControlBox = styled.div`
@@ -190,13 +368,20 @@ const StyledControlBox = styled.div`
   width: 310px;
 `;
 
-const StyledServiceCardsContainer = styled.div<{ itemsPerPage: number }>`
+interface StyledServiceCardsContainerProps {
+  currentIndex: number;
+  services: any[];
+}
+
+const StyledServiceCardsContainer = styled.div<StyledServiceCardsContainerProps>`
   display: flex;
-  flex-wrap: nowrap;
   gap: 15px;
-  width: ${(props) => props.itemsPerPage * (310 + 15)}px; /* Adjusted width */
-  margin-left: auto;
+  height: 100%;
   box-sizing: content-box;
+  transform: translateX(
+    ${(props) => -props.currentIndex * (210 / props.services.length)}%
+  );
+  transition: transform 0.3s ease-in-out;
 `;
 
 export default Services;
--- src/theme/components/organisms/Steps/index.tsx
+++ src/theme/components/organisms/Steps/index.tsx
@@ -7,27 +7,106 @@ import Flex from "../../atoms/Flex";
 const steps = [
   {
     label: "Pasul 1",
-    title: "Completează Chestionarul",
+    title: "Completarea Chestionarului de Evaluare",
     image: "/assets/img1.jpg",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, repudiandae quis laudantium obcaecati rerum vitae enim aperiam eum veniam eaque.",
+    desc: (
+      <>
+        Începeți experiența noastră personalizată prin a completa chestionarul
+        de evaluare detaliat.
+        <br />
+        <br />
+        Aceste întrebări informatice și relevante ne vor ajuta să înțelegem mai
+        bine nevoile și preocupările dumneavoastră specifice în ceea ce privește
+        starea neruo-musculo-scheletală.
+        <br />
+        <br />
+        Asigurându-ne că avem informațiile corecte, putem adapta tratamentul și
+        intervențiile noastre pentru a vă oferi cea mai eficientă și
+        personalizată îngrijire.
+        <br />
+        <br />
+        Pasul acesta esențial reprezintă prima etapă către o îmbunătățire
+        durabilă a stării dumneavoastră de sănătate.
+      </>
+    ),
   },
   {
     label: "Pasul 2",
-    title: "Titlu pasul 2",
+    title: "Perioada de Așteptare și Analiză a Informațiilor",
     image: "/assets/img2.jpg",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, repudiandae quis laudantium obcaecati rerum vitae enim aperiam eum veniam eaque.",
+    desc: (
+      <>
+        După ce ați completat cu atenție chestionarul de evaluare, vă rugăm să
+        ne permiteți o perioadă de așteptare.
+        <br />
+        <br />
+        Echipa noastră de specialiști va analiza cu atenție informațiile
+        furnizate, evaluând detaliile cu privire la starea
+        neruo-musculo-scheletală și necesitățiile dumneavoastră individuale.
+        <br />
+        <br />
+        Această fază de analiză este crucială pentru a dezvolta un plan
+        personalizat de tratament.
+        <br />
+        <br />
+        Vă vom contacta în curând pentru a discuta rezultate și pentru a stabili
+        pașii următori în îngrijirea dumneavoastră.
+        <br />
+        <br />
+        Vă mulțumim pentru răbdare și colaborare în procesul nostru de oferire a
+        serviciilor de calitate.
+      </>
+    ),
   },
   {
     label: "Pasul 3",
-    title: "Titlu pasul 3",
+    title: "Programarea Telefonică și Detalierea Costurilor",
     image: "/assets/img3.jpg",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, repudiandae quis laudantium obcaecati rerum vitae enim aperiam eum veniam eaque.",
+    desc: (
+      <>
+        În această etapă, un membru al echipei noastre va lua legătura cu
+        dumneavoastră pentru a stabili o programare.
+        <br />
+        <br />
+        Vom discuta rezultatele analizei și vom oferi clarificări privind
+        costurile și modalitățile de plată asociate tratamentului.
+        <br />
+        <br />
+        În timpul acestei convorbiri, ne dorim să vă asigurăm de transparența
+        procesului nostru, explicând fiecare aspect al planului de tratament
+        propus și răspunzând la orice întrebări aveți.
+        <br />
+        <br />
+        Obiectivul nostru este să vă oferim informații complete pentru a vă
+        ajuta să luați decizii înțelepte și să vă simțiți confortabil cu pașii
+        următori în îngrijirea dumneavoastră.
+      </>
+    ),
   },
   {
     label: "Pasul 4",
-    title: "Titlu pasul 4",
+    title: "Efectuarea Tratamentului la Domiciliu",
     image: "/assets/img4.jpg",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, repudiandae quis laudantium obcaecati rerum vitae enim aperiam eum veniam eaque.",
+    desc: (
+      <>
+        Odată ce programul de Kinetoterapie este stabilit, vă îndrumăm să vă
+        pregătiți pentru a efectua tratamentul la domiciliu.
+        <br />
+        <br />
+        Unul dintre specialiștii noștri vă va ghida cu atenție prin exercițiile
+        și tehnici specifice, asigurându-se că sunteți confortabil și că
+        înțelegeți corect fiecare aspect al programului dumneavoastră de
+        recuperare.
+        <br />
+        <br />
+        Respectăm orarul stabilit, adaptându-ne nevoilor dumneavoastră pentru a
+        vă asigura că beneficiați la maxim de fiecare sesiune de kinetoterapie.
+        <br />
+        <br />
+        Acest pas marchează începutul colaborării noastre continue pentru a
+        îmbunătăți starea dumneavoastră de sănătate la domiciliu.
+      </>
+    ),
   },
 ];
 
@@ -72,7 +151,7 @@ const Steps = () => {
 const StyledStepsBox = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 60px;
+  gap: 80px;
   align-items: center;
   justify-content: center;
   > * {
