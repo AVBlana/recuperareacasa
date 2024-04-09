ecd4cd58ff5e70d4f9d6bc2c7fd1be73dd840f3f
AVBlana <71121584+AVBlana@users.noreply.github.com>
2024-04-09 14:54:54 +0300

    moved favicon files in public from public/favicon, changed all paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,37 +24,37 @@ export default function App({ Component, pageProps }: AppProps) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="https://www.recuperareacasa.ro/favicon/apple-touch-icon.png"
+          href="https://www.recuperareacasa.ro/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="https://www.recuperareacasa.ro/favicon/favicon-32x32.png"
+          href="https://www.recuperareacasa.ro/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="https://www.recuperareacasa.ro/favicon/favicon-16x16.png"
+          href="https://www.recuperareacasa.ro/favicon-16x16.png"
         />
         <link
           rel="manifest"
-          href="https://www.recuperareacasa.ro/favicon/site.webmanifest"
+          href="https://www.recuperareacasa.ro/site.webmanifest"
         />
         <link
           rel="mask-icon"
-          href="https://www.recuperareacasa.ro/favicon/safari-pinned-tab.svg"
+          href="https://www.recuperareacasa.ro/safari-pinned-tab.svg"
           color="#5bbad5"
         />
         <link
           rel="shortcut icon"
-          href="https://www.recuperareacasa.ro/favicon/favicon.ico"
+          href="https://www.recuperareacasa.ro/favicon.ico"
         />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta
           name="msapplication-config"
-          content="https://www.recuperareacasa.ro/favicon/browserconfig.xml"
+          content="https://www.recuperareacasa.ro/browserconfig.xml"
         />
         <meta name="theme-color" content="#ffffff" />
       </Head>
